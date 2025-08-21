39d5d30089c5cf781ad36eee6c851c36fd76f0be
shahadot99999 <shahadot99999@gmail.com>
2025-08-21 22:47:01 +0600

    Auth Provider is modify something

--- src/provider/AuthProvider.jsx
+++ src/provider/AuthProvider.jsx
@@ -3,9 +3,7 @@ import React, { createContext, useState } from 'react';
 export const AuthContext =createContext();
 
 const AuthProvider = ({children}) => {
-    const [user, setuser]= useState({
-        name: "Shahadot"
-    });
+    const [user, setuser]= useState(null);
 
     const authInfo = {
         user,
